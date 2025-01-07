58a5582168fcafa10fecca99a5471e7dec058d35
Rajesh97266 <rajesh97266@gmail.com>
2025-01-07 22:30:20 +0530

    Header-Footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
 import Home from './pages/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
    <div className="App">
+    <Header/>
    <Home />
+   <Footer/>
    </div>
   )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,41 +2,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Fragment } from "react";
 
+
 export default function Home() {
     return (
       <Fragment>
-        <nav className="navbar row">
-          <div className="col-12 col-md-3">
-            <div className="navbar-brand">
-              <img width="150px" src="/images/logo.png" />
-            </div>
-          </div>
-
-          <div className="col-12 col-md-6 mt-2 mt-md-0">
-            <div className="input-group">
-              <input
-                type="text"
-                id="search_field"
-                className="form-control"
-                placeholder="Enter Product Name ..."
-              />
-              <div className="input-group-append">
-                <button id="search_btn" className="btn">
-                  <i className="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-            <span id="cart" className="ml-3">
-              Cart
-            </span>
-            <span className="ml-1" id="cart_count">
-              2
-            </span>
-          </div>
-        </nav>
+      
 
         <h1 id="products_heading">Latest Products</h1>
 
@@ -182,11 +152,7 @@ export default function Home() {
           </div>
         </section>
 
-        <footer className="py-1 bg-dark">
-          <p className="text-center text-white mt-1 ">
-            E-Commerce - 2023-2024, All Rights Reserved
-          </p>
-        </footer>
+       
       </Fragment>
     );
 }
