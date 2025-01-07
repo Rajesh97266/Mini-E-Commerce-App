7c85818610ee9dfbe04c1af8d01f408eb7dfc155
Rajesh97266 <rajesh97266@gmail.com>
2025-01-07 14:53:31 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const path =  require("path");
+
+dotenv.config({path:path.join(__dirname,'config','.env')});
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
